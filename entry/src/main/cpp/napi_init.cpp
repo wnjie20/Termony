16375006a192392a9473297edd97495d3b07c3f3
Jiajie Chen <c@jia.je>
2025-06-08 13:38:09 +0800

    Add initial opengl render

--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <pty.h>
 #include <poll.h>
+#include <EGL/egl.h>
+#include <native_window/external_window.h>
+#include <GLES3/gl32.h>
 
 static int fd = -1;
 
@@ -79,13 +82,76 @@ static napi_value Send(napi_env env, napi_callback_info info)
     return nullptr;
 }
 
+static napi_value CreateSurface(napi_env env, napi_callback_info info)
+{
+    size_t argc = 1;
+    napi_value args[1] = {nullptr};
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+
+    int64_t surface_id = 0;
+    bool lossless = true;
+    assert(napi_get_value_bigint_int64(env, args[0], &surface_id, &lossless) == napi_ok);
+
+    OHNativeWindow *native_window;
+    OH_NativeWindow_CreateNativeWindowFromSurfaceId(surface_id, &native_window);
+    assert(native_window);
+    EGLNativeWindowType eglWindow_ = (EGLNativeWindowType)native_window;
+    EGLDisplay eglDisplay_ = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    assert(eglDisplay_ != EGL_NO_DISPLAY);
+
+    EGLint majorVersion;
+    EGLint minorVersion;
+    assert(eglInitialize(eglDisplay_, &majorVersion, &minorVersion) == EGL_TRUE);
+
+    const EGLint attrib[] = {
+        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_ALPHA_SIZE, 8,
+        EGL_DEPTH_SIZE, 24,
+        EGL_STENCIL_SIZE, 8,
+        EGL_SAMPLE_BUFFERS, 1,
+        EGL_SAMPLES, 4, // Request 4 samples for multisampling
+        EGL_NONE
+    };
+
+    const EGLint maxConfigSize = 1;
+    EGLint numConfigs;
+    EGLConfig eglConfig_;
+    assert(eglChooseConfig(eglDisplay_, attrib, &eglConfig_, maxConfigSize, &numConfigs) == EGL_TRUE);
+
+    EGLSurface eglSurface_ = eglCreateWindowSurface(eglDisplay_, eglConfig_, eglWindow_, NULL);
+
+    EGLint contextAttributes[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE };
+    EGLContext eglContext_ = eglCreateContext(eglDisplay_, eglConfig_, EGL_NO_CONTEXT, contextAttributes);
+
+    eglMakeCurrent(eglDisplay_, eglSurface_, eglSurface_, eglContext_);
+
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+    glFinish();
+    eglSwapBuffers(eglDisplay_, eglSurface_);
+
+    return nullptr;
+}
+
+static napi_value DestroySurface(napi_env env, napi_callback_info info)
+{
+    return nullptr;
+}
+
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports)
 {
     napi_property_descriptor desc[] = {
         { "run", nullptr, Run, nullptr, nullptr, nullptr, napi_default, nullptr },
         { "read", nullptr, Read, nullptr, nullptr, nullptr, napi_default, nullptr },
-        { "send", nullptr, Send, nullptr, nullptr, nullptr, napi_default, nullptr }
+        { "send", nullptr, Send, nullptr, nullptr, nullptr, napi_default, nullptr },
+        { "createSurface", nullptr, CreateSurface, nullptr, nullptr, nullptr, napi_default, nullptr },
+        { "destroySurface", nullptr, DestroySurface, nullptr, nullptr, nullptr, napi_default, nullptr },
     };
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
--- entry/src/main/cpp/types/libentry/Index.d.ts
+++ entry/src/main/cpp/types/libentry/Index.d.ts
@@ -1,3 +1,5 @@
 export const run: () => void;
 export const read: () => ArrayBuffer | undefined;
 export const send: (content: ArrayBuffer) => void;
+export const createSurface: (id: BigInt) => void;
+export const destroySurface: (id: BigInt) => void;
