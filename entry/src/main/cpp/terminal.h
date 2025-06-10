6c024248b2cc756516e4171d73029ee058af9e29
Jiajie Chen <c@jia.je>
2025-06-11 00:29:42 +0800

    Refactor code

---
+++ entry/src/main/cpp/terminal.h
@@ -0,0 +1,20 @@
+#ifndef __TERMINAL_H__
+#define __TERMINAL_H__
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// start a terminal
+void Start();
+// start rendering
+void StartRender();
+// send data to terminal
+void SendData(uint8_t *data, size_t length);
+// resize window
+void Resize(int width, int height);
+void ScrollBy(double offset);
+
+extern void BeforeDraw();
+extern void AfterDraw();
+
+#endif
