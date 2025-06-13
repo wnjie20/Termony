061d5feb9862581845efc4b3ba89e4b96c476188
Jiajie Chen <c@jia.je>
2025-06-13 10:12:59 +0800

    Add basic test

--- entry/src/main/cpp/test.cpp
+++ entry/src/main/cpp/test.cpp
@@ -1,14 +1,15 @@
 // build with:
 // g++ test.cpp terminal.cpp -I/usr/include/freetype2 -DSTANDALONE -DTESTING -o test -lCatch2Main -lCatch2 -lfreetype -lGLESv2 -lglfw
+#include "terminal.h"
 #include <catch2/catch_test_macros.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+TEST_CASE( "Columns change with input", "" ) {
+    terminal_context ctx;
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    ctx.ResizeTo(80, 24);
+    REQUIRE( ctx.row == 0 );
+    REQUIRE( ctx.col == 0 );
+    ctx.Parse('a');
+    REQUIRE( ctx.row == 0 );
+    REQUIRE( ctx.col == 1 );
 }
